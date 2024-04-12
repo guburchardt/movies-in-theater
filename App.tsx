c20c00880538a9eef60875ac5ea9ed1ac05cd81a
guburchardt <guburchardt@hotmail.com>
2024-04-12 18:07:29 -0300

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Testando 1,2,3...</Text>
+    </SafeAreaView>
+  );
+}
+
